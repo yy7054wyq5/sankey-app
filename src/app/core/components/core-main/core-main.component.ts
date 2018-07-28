2cf2da6b0bef46ed902ae746cf38230558549b6e
wuyang <yang_wu@loongjoy.com>
2018-07-28 09:26:42 +0800

    布局

--- src/app/core/components/core-main/core-main.component.ts
+++ src/app/core/components/core-main/core-main.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-core-main',
   templateUrl: './core-main.component.html',
-  styleUrls: ['./core-main.component.less']
+  styleUrls: ['./core-main.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CoreMainComponent implements OnInit {
 
