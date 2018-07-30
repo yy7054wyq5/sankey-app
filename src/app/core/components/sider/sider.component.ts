98dd6c8031bcddbc226ce18bac3952097abf264e
wuyang <yang_wu@loongjoy.com>
2018-07-30 23:07:31 +0800

    侧栏

--- src/app/core/components/sider/sider.component.ts
+++ src/app/core/components/sider/sider.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { siderMenus, SideMenuItem} from '../../config';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { siderMenus, SideMenuItem } from '../../config';
 
 
 @Component({
   selector: 'app-sider',
   templateUrl: './sider.component.html',
-  styleUrls: ['./sider.component.less']
+  styleUrls: ['./sider.component.less'],
 })
 export class SiderComponent implements OnInit {
 
+  open = false;
   siderMenus: SideMenuItem[] = siderMenus;
 
   constructor() { }
