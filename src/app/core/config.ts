0065c61f67bffb69d8d0c4cc4e5c32e1661f5773
wuyang <yang_wu@loongjoy.com>
2018-07-31 14:19:37 +0800

    侧栏

--- src/app/core/config.ts
+++ src/app/core/config.ts
@@ -5,23 +5,38 @@ export interface SideMenuItem {
   actived: boolean;
 }
 
-const siderMenus: SideMenuItem[] = [{
-  txt: '人物信息',
-  actived: false,
-  icon: 'user',
-  className: 'user-info'
-}, {
-  txt: '历史信息',
-  actived: false,
-  icon: 'history',
-  className: 'history-info'
-}];
+const siderMenus: SideMenuItem[] = [
+  {
+    txt: '人物信息',
+    actived: false,
+    icon: 'user',
+    className: 'user-info'
+  },
+  {
+    txt: '历史信息',
+    actived: false,
+    icon: 'history',
+    className: 'history-info'
+  }
+];
 
 const chartColorConfig = {
-  humen: '#a7ccf3',
-  event: '#f4bb8b',
-  company: '#2aace3',
-  point: '#2702bb'
+  humen: {
+    bg: '#f4b98c',
+    hover: '#f4b98c'
+  },
+  event: {
+    bg: '#f4b98c',
+    hover: '#ff6700'
+  },
+  company: {
+    bg: '#ff6700',
+    hover: '#ff6700'
+  },
+  point: {
+    bg: '#ff6700',
+    hover: '#ff6700'
+  }
 };
 
 const chartOption = {
