c26f81c80a01c4cb72b5309597f56304e6c98334
wuyang <yang_wu@loongjoy.com>
2018-08-03 16:28:48 +0800

    全屏请求遮罩

--- src/app/share/services/interceptors/interceptors.service.ts
+++ src/app/share/services/interceptors/interceptors.service.ts
@@ -2,14 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, tap } from 'rxjs/operators';
 import { environment } from '../../../../environments/environment';
+import { LoadingService } from '../loading/loading.service';
 
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class InterceptorsService implements HttpInterceptor {
+  constructor(private _loading: LoadingService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this._loading.open();
     // 开发环境将请求延迟500ms，以查看loading效果是否有效
-    return next.handle(req).pipe(delay(environment.requestDelayTime));
+    return next.handle(req).pipe(
+      delay(environment.requestDelayTime),
+      tap(
+        res => {
+          this._loading.close();
+        },
+        error => {
+          this._loading.close();
+        },
+        () => {
+          this._loading.close();
+        }
+      )
+    );
   }
 }
--- src/app/share/services/loading/loading.service.ts
+++ src/app/share/services/loading/loading.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
+const loadingStyle = 'position: fixed;left: 0;top: 0;width: 100vw;height: 100vh; z-index:9';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
+  constructor() {}
+  loadingShadow: Element;
 
-  constructor() { }
+  open() {
+    if (!this.loadingShadow) {
+      this.loadingShadow = document.createElement('div');
+      this.loadingShadow.setAttribute('style', loadingStyle);
+      document.querySelector('body').appendChild(this.loadingShadow);
+    } else {
+      this.loadingShadow.setAttribute('style', loadingStyle);
+    }
+  }
+
+  close() {
+    this.loadingShadow.setAttribute('style', 'display: none;');
+  }
 }
